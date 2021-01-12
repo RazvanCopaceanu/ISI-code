740d50d8ce3a97e09463fe81bf4bda381fdbbc5c
razvan <razvan.copaceanu19@gmail.com>
2021-01-12 11:27:06 +0100

    Añadidas librerías para descompresión de fichero

--- unzipFile.java
+++ unzipFile.java
@@ -1,34 +1,57 @@
-public class UnzipFile {
-    public static void main(String[] args) throws IOException {
-        String fileZip = "src/main/resources/unzipTest/compressed.zip";
-        File destDir = new File("src/main/resources/unzipTest");
-        byte[] buffer = new byte[1024];
-        ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip));
-        ZipEntry zipEntry = zis.getNextEntry();
-        while (zipEntry != null) {
-        	File newFile = newFile(destDir, zipEntry);
-     		if (zipEntry.isDirectory()) {
-         		if (!newFile.isDirectory() && !newFile.mkdirs()) {
-             			throw new IOException("Failed to create directory " + newFile);
-         		}
-     		} else {
-         		// fix for Windows-created archives
-         		File parent = newFile.getParentFile();
-         		if (!parent.isDirectory() && !parent.mkdirs()) {
-             			throw new IOException("Failed to create directory " + parent);
-         		}
-         
-         		// write file content
-         		FileOutputStream fos = new FileOutputStream(newFile);
-         		int len;
-         		while ((len = zis.read(buffer)) > 0) {
-             			fos.write(buffer, 0, len);
-         		}
-         		fos.close();
-     		}
-	zipEntry = zis.getNextEntry();
-        }
-        zis.closeEntry();
-        zis.close();
+package com.journaldev.files;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class UnzipFiles {
+
+    public static void main(String[] args) {
+        String zipFilePath = "/Users/pankaj/tmp.zip";
+        
+        String destDir = "/Users/pankaj/output";
+        
+        unzip(zipFilePath, destDir);
     }
-} 
+
+    private static void unzip(String zipFilePath, String destDir) {
+        File dir = new File(destDir);
+        // create output directory if it doesn't exist
+        if(!dir.exists()) dir.mkdirs();
+        FileInputStream fis;
+        //buffer for read and write data to file
+        byte[] buffer = new byte[1024];
+        try {
+            fis = new FileInputStream(zipFilePath);
+            ZipInputStream zis = new ZipInputStream(fis);
+            ZipEntry ze = zis.getNextEntry();
+            while(ze != null){
+                String fileName = ze.getName();
+                File newFile = new File(destDir + File.separator + fileName);
+                System.out.println("Unzipping to "+newFile.getAbsolutePath());
+                //create directories for sub directories in zip
+                new File(newFile.getParent()).mkdirs();
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                fos.write(buffer, 0, len);
+                }
+                fos.close();
+                //close this ZipEntry
+                zis.closeEntry();
+                ze = zis.getNextEntry();
+            }
+            //close last ZipEntry
+            zis.closeEntry();
+            zis.close();
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+}
